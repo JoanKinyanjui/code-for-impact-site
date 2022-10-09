f7a6be3f56ea7bbc7edd399c80ed5d1c588e53a9
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-10-09 21:11:44 +0300

    responsiveness

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,13 @@ import '../styles/global.css'
 
 const Nav = () =>{
 return(
-    <div className='w-screen pb-5 flex shadow-md font-serif fixed bg-white'>
-    <div className='w-1/4 '>
-        <img  className=' pt-8 sm:pt-8 md:pt-4 w-full sm:w-1/2 mx-auto' alt='img'  src= {tree} />
+    <div className='w-screen  flex shadow-md font-serif fixed bg-white '>
+    <div className='border-solid mx-auto'>
+        <img  className='mx-auto w-3/4  sm:w-full h-20 sm:h-24 md:h-29 lg:h-32 xl:h-34 ' alt='img'  src= {tree} />
     </div>
 
-    <div className='w-3/4 sm:w-1/2 text-center text-sm sm:text-lg mt-16 sm:mt-10'>
-        <ul className='flex flex-row flex space-x-3 sm:space-x-10 list-none text-center justify-center'>
+    <div className='text-center text-sm sm:text-lg   mx-auto'>
+        <ul className='flex flex-row flex space-x-3 sm:space-x-10 list-none text-center justify-center mt-8 sm:mt-10 lg:mt-14'>
             <li className=''>Features</li>
             <li>Services</li>
             <li>Stats</li>
@@ -18,8 +18,8 @@ return(
         </ul>
     </div>
 
-    <div className=' self-end lg:self-start ml-auto mt-10 w-1/8 lg:w-1/4'>
-    <button class="bg-blue-400 hover:bg-blue-700 hover:text-lg text-sm text-white  sm:font-bold py-0 sm:py-2 px-2 sm:px-6 rounded"> Login </button>
+    <div className='mt-8 sm:mt-10 lg:mt-14 mx-auto'>
+    <button class="bg-blue-400 hover:bg-blue-700 hover:text-lg text-sm text-white  sm:font-bold rounded  px-1 py1 md:px-6 md:py-2"> Login </button>
 
     </div>
     </div>
