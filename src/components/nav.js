67a698fa4489ff0dcae0a3d90870b5e33d2777de
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-10-09 21:55:57 +0300

    .

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,17 @@ import '../styles/global.css'
 
 const Nav = () =>{
 return(
-    <div className='w-screen  flex shadow-md font-serif fixed bg-white '>
-    <div className='border-solid mx-auto'>
-        <img  className='mx-auto w-3/4  sm:w-full h-20 sm:h-24 md:h-29 lg:h-32 xl:h-34 ' alt='img'  src= {tree} />
+    <div className='w-screen  block sm:flex shadow-md font-serif fixed bg-white '>
+<div className='mx-auto sm:w-1/8  '>
+<div className='border-solid mx-auto'>
+        <img  className='mx-auto sm:w-full h-10 sm:h-20 sm:h-24 md:h-29 lg:h-32 xl:h-34 ' alt='img'  src= {tree} />
     </div>
+</div>
 
-    <div className='text-center text-sm sm:text-lg   mx-auto'>
-        <ul className='flex flex-row flex space-x-3 sm:space-x-10 list-none text-center justify-center mt-8 sm:mt-10 lg:mt-14'>
+    <div className='flex mx-auto w-screen sm:w-3/4'>
+
+    <div className='text-center text-xs sm:text-sm  md:text-base mx-auto w-screen w-full'>
+        <ul className='flex flex-row flex space-x-2 sm:space-x-8 list-none text-center justify-center mt-8 sm:mt-10 lg:mt-14'>
             <li className=''>Features</li>
             <li>Services</li>
             <li>Stats</li>
@@ -18,8 +22,10 @@ return(
         </ul>
     </div>
 
-    <div className='mt-8 sm:mt-10 lg:mt-14 mx-auto'>
+    <div className='mt-8 sm:mt-10 lg:mt-14 mx-auto sm:mb-0 mb-1 '>
     <button class="bg-blue-400 hover:bg-blue-700 hover:text-lg text-sm text-white  sm:font-bold rounded  px-1 py1 md:px-6 md:py-2"> Login </button>
+
+    </div>
 
     </div>
     </div>
