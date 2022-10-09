b582fdd76209989ab2a0e50dda898c1c90d66d0e
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-10-09 10:01:34 +0300

    uncommented theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ module.exports = {
     title: `Code For Impact`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // theme:{
-  //   screens:{
-  //     "sm" : "640px",
-  //     "md":"768px",
-  //     "lg":"1024px",
-  //    "xl":"1280px",
-  //   }
-  // },
+  theme:{
+    screens:{
+      "sm" : "640px",
+      "md":"768px",
+      "lg":"1024px",
+     "xl":"1280px",
+    }
+  },
   plugins:[],
   plugins: [`gatsby-transformer-sharp`, `gatsby-plugin-sharp`],
   plugins: ['gatsby-plugin-postcss'],
